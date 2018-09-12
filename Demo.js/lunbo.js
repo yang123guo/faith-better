9b9182ea5b4d8c9197cf329762be84861ade01e7
yanggqm <289656703@qq.com>
2018-09-12 23:44:16 +0800

    dfg

---
+++ Demo.js/lunbo.js
@@ -0,0 +1,15 @@
+
+export function playNext(type = 'next') {
+    let index = getCurrentIndex();
+    const length = [].length;
+    let newIndex = null;
+
+    if(type === 'next') {
+        index ++;
+        newIndex = index % length;  // 这里是精髓
+    }else {
+        index --;
+        newIndex = (index + length) % length;  // 这里是精髓
+    }
+    doNext(newIndex)
+}
