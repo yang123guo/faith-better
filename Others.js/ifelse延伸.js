3ac446d11e7eda2df0b531cf1c2dae6afba95590
yanggqm <289656703@qq.com>
2018-11-10 23:58:50 +0800

    y

--- Others.js/ifelse延伸.js
+++ Others.js/ifelse延伸.js
@@ -1,4 +1,16 @@
 /**
- *  if else 不同条件走不同情况
- *  switch case 可以替代
+ *  1、if else 不同条件走不同情况
+ *  2、switch case 可以替代
+ *  3、还可以用对象不同的键来替代
+ *  4、
  */ 
+
+
+if(status == 1){
+    sendLog('processing')
+    jumpTo('IndexPage')
+}else if(status == 2){
+    sendLog('fail')
+    jumpTo('FailPage')
+}
+
